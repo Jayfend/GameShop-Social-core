2cbe9c0cd44b5bd409930d3e1d7cb7668c942351
Phung Le Luan <66172952+Jayfend@users.noreply.github.com>
2022-12-16 22:00:13 +0700

    Thống kê theo tháng, năm

---
+++ GameShop/Controllers/ChartsController.cs
@@ -0,0 +1,26 @@
+﻿using GameShop.Application.Catalog.Charts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GameShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+        private readonly IChartService _chartService;
+
+        public ChartsController(IChartService chartService)
+        {
+            _chartService = chartService;
+        }
+
+        [HttpGet("BestSellerPerMonth/{Year}/{Month}")]
+        public async Task<IActionResult> GameStatisticalByMonthAndYear(int Year, int Month)
+        {
+            var result = await _chartService.GameStatisticalByMonthAndYear(Year, Month);
+            return Ok(result);
+        }
+    }
+}

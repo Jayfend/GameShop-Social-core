3f3b8f14a31a3221751a3e8639c076fce70f0592
Luan Nguyen Phung Le <LuanNPL@fpt.com.vn>
2023-05-17 16:09:22 +0700

    dsadas

--- GameShop.ViewModel/System/Users/LoginRequest.cs
+++ GameShop.ViewModel/System/Users/LoginRequest.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GameShop.ViewModels.System.Users
 {
     public class LoginRequest
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Code { get; set; }
         public bool RememberMe { get; set; }
     }
